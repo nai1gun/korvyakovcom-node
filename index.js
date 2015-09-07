e67aa799dd6ca61c1f90bc1aa09d0f13b57c5f64
Nailgun <djkp9ik@gmail.com>
2015-09-08 01:32:39 +0300

    [+] all pages

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ app.get('/', function(request, response) {
   response.render('pages/index', {title: 'Alexander Korvyakov, software engeneer'});
 });
 
+app.get('/about', function(request, response) {
+  response.render('pages/about', {title: 'Alexander Korvyakov, software engeneer - about'});
+});
+
+app.get('/education', function(request, response) {
+  response.render('pages/education', {title: 'Alexander Korvyakov, software engeneer - education'});
+});
+
+app.get('/experience', function(request, response) {
+  response.render('pages/experience', {title: 'Alexander Korvyakov, software engeneer - experience'});
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects', {title: 'Alexander Korvyakov, software engeneer - projects'});
+});
+
+app.get('/skills', function(request, response) {
+  response.render('pages/skills', {title: 'Alexander Korvyakov, software engeneer - skills'});
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });

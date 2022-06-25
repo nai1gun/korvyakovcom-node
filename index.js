86eec3ff50e671d154fbc42cabbf917d59c9615f
Alexander <djkp9ik@gmail.com>
2022-06-26 01:51:17 +0300

    url shortener for the 'Road Safety' article

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.get('/structuring-ml', function(req, res) {
   res.redirect('https://www.tomtom.com/blog/location-technology/structuring-machine-learning/');
 });
 
+
+app.get('/road-safety', function(req, res) {
+  res.redirect('https://www.linkedin.com/pulse/why-machine-learning-development-should-always-agile-korvyakov/');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
